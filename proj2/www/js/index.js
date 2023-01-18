6420fe263bcc61deae6b27afad681a3b62791527
igonzalez-et <77338435+igonzalez-et@users.noreply.github.com>
2023-01-18 18:14:49 +0100

    Finalitzat

--- proj2/www/js/index.js
+++ proj2/www/js/index.js
@@ -29,17 +29,46 @@ function onDeviceReady() {
 }
 
 let storageTasks = JSON.parse(localStorage.getItem("tasks"));
-for (let i = 0; i < storageTasks.length; i++) {
-    $("ul").append("<li><a href='#page1'>"+storageTasks[i]+"<button class='buttonEliminar'>Eliminar</button></a></li>");
+if(storageTasks == null){
+    storageTasks = [];
+}
+else{
+    for (let i = 0; i < storageTasks.length; i++) {
+        var elem = $("ul").append("<li><a href='#page1'>"+storageTasks[i]+"<button class='buttonEliminar' data-role='none'>Eliminar</button></a></li>");
+    }
+    $("#homePage").ready(function(){
+        $("a", elem).click(editar);
+        $("ul").listview( "refresh" );
+        $('ul li a button').click(function(e){
+            var tar = e.target || e.srcElement;
+            let textoListView = $(tar.parentElement).text();
+            for (let i = 0; i < storageTasks.length; i++) {
+                if(textoListView == storageTasks[i]+"Eliminar"){
+                    storageTasks.splice(i,1);
+                    localStorage.setItem("tasks", JSON.stringify(storageTasks));
+                }
+            }
+            $(tar.parentElement).remove();
+            return false;
+        });
+    });
+    
 }
 
-let botonAdd = $("#afegir").click(function() {
+$("#afegir").click(function() {
     let pro = prompt("Escribe el nombre de la tarea que quieres añadir:");
     var elem = $("ul").append("<li><a href='#page1'>"+pro+"<button class='buttonEliminar'>Eliminar</button></a></li>");
     $("a", elem).click(editar);
     $("ul").listview( "refresh" );
-    $('ul li button').click(function(e){
+    $('ul li a button').click(function(e){
         var tar = e.target || e.srcElement;
+        let textoListView = $(tar.parentElement).text();
+        for (let i = 0; i < storageTasks.length; i++) {
+            if(textoListView == storageTasks[i]+"Eliminar"){
+                storageTasks.splice(i,1);
+                localStorage.setItem("tasks", JSON.stringify(storageTasks));
+            }
+        }
         $(tar.parentElement).remove();
         return false;
     });
@@ -52,11 +81,21 @@ var targetElem = null;
 function editar(e){
     var tar = e.target || e.srcElement;
     targetElem = tar;
+
 }
 
 $("#guardaButton").click(guardar);
 function guardar(){
     var editTaskList = $("#editName").val();
+
+    let textoListView = $(targetElem.parentElement).text();
+    for (let i = 0; i < storageTasks.length; i++) {
+        if(textoListView == storageTasks[i]+"Eliminar"){
+            storageTasks[i] = editTaskList;
+            localStorage.setItem("tasks", JSON.stringify(storageTasks));
+        }
+    }
+
     botoStr = "<button class='buttonEliminar'>Eliminar</button>";
     $(targetElem).html(editTaskList+botoStr);
     $('ul li button').click(function(e){
